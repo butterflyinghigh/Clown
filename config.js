f90070ee7886bef0064272d9e95cc26d997525fd
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-19 20:09:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://drive.google.com/file/d/1a316EIunEEAOkvvd3_bkZQIfxqg2fs6U/view?usp=sharing",
+ALIVE_MSG: process.env.ALIVE_MSG || "> *👋 Hi*\n> *🤖 I am Clown-MD Whatsapp Bot*\n> *✅ I am Alive Now!*\n\n\n${botwatermark}",
 };
-
