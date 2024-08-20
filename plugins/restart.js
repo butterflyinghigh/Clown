7ca23176465617b6296c5195ee5a82a0862bc50a
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-20 20:36:43 +0530

    Update restart.js

--- plugins/restart.js
+++ plugins/restart.js
@@ -1,7 +1,7 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
 const {sleep} = require('../lib/functions')
-const { botwatermark } = require('./botwatermark')
+const { botwatermark } = require('../botwatermark')
 
 cmd({
     pattern: "restart",
