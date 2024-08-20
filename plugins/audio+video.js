f95f47c2f7644b28a8c6638f5aba8d7ba9cdcf25
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-20 20:36:09 +0530

    Update audio+video.js

--- plugins/audio+video.js
+++ plugins/audio+video.js
@@ -1,7 +1,7 @@
 const {cmd , commands} = require('../command')
 const fg = require('api-dylux')
 const yts = require('yt-search')
-const { botwatermark } = require('./botwatermark')
+const { botwatermark } = require('../botwatermark')
 
 cmd({
     pattern: "song",
