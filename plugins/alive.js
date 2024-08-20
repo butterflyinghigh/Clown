ba4175772371c328fc9efa7a11f6ab60ac1fb09f
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-20 20:12:45 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,5 +1,6 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
+const { botwatermark } = require('./botwatermark')
 
 cmd({
     pattern: "alive",
@@ -9,7 +10,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG + botwatermark},{quoted: mek})
 }catch(e){
 console.log(e)
 reply(`${e}`)
