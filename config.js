41a4de9a06958ca252fdd368a96a878cbcc517a8
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-20 02:41:03 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "2Ms1zbaA#wP2T5Cxnu2lCuz65SQ5KFs8BK6C-JPWJkcbDDiMO5VI",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://drive.google.com/file/d/1a316EIunEEAOkvvd3_bkZQIfxqg2fs6U/view?usp=sharing",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/9e0a59ba5c8cd8c292741.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "> *👋 Hi*\n> *🤖 I am Clown-MD Whatsapp Bot*\n> *✅ I am Alive Now!*\n\n\n" + botwatermark,
 };
