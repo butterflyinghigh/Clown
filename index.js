c7726c348c406a856738d0756d18ead718dd1612
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-20 20:16:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
 const prefix = '.'
+const { botwatermark } = require('./botwatermark')
 
 const ownerNumber = ['94755845165']
 
@@ -69,7 +70,7 @@ require("./plugins/" + plugin);
 console.log('✅ Clown-MD Plugins Installed')
 console.log('✅ Clown-MD Bot Connected')
 
-let up = `> *🤖 Clown-MD Whatsapp Bot Connected ✅*\n\n*PREFIX:* ${prefix}`;
+let up = `> *🤖 Clown-MD Whatsapp Bot Connected ✅*\n\n*PREFIX:* ${prefix}\n\n\n` + botwatermark;
 
 conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://telegra.ph/file/70222cf39588987d83f51.png` }, caption: up })
 
