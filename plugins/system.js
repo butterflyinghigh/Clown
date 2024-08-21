37e919be125556c8248a4db0c8e3515c9ef943fd
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-21 21:36:18 +0530

    Update system.js

--- plugins/system.js
+++ plugins/system.js
@@ -1,12 +1,30 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
+const os = require(os)
+const {runtime} = require('../lib/functions')
 const { botwatermark } = require('../botwatermark')
 
 cmd({
-    pattern: "alive",
-    desc: "Check bot online or offline",
+    pattern: "system",
+    alias: [status],
+    desc: "Check system informations",
     category: "main",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+
+let status = `
+*Uptime:*  ${runtime(process.uptime())}
+*Ram:* ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+*Platform:* ${os.hostname()}
+*Owner:* Mr.Unknown
+*Bot Name:* Clown-MD\n\n\n${botwatermark}`
+
+return await conn.sendMessages(from,{image{url: ``},caption:status},{qouted:mek})
+    
+}catch(e)
+console.log(e)
+reply(`${e}`)
+}
+})
