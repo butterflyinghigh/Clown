80ac0876709612c064ecaf55344162052db735ce
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-21 19:27:13 +0530

    Update gpt.js

--- plugins/gpt.js
+++ plugins/gpt.js
@@ -12,8 +12,8 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-data = await fetchJson('https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}')
-return reply('data.data')
+data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
+return reply(`${data.data} + botwatermark`)
   
 }catch(e){
 console.log(e)
