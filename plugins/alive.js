4eb3a2aa03141ce009e9784ffde970a3f42c9e07
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-22 07:10:13 -0700

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -11,8 +11,8 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
        await conn.sendMessage(from,{audio:{url: config.ALIVE_AUDIO},mimetype:"audio/mpeg",ptt:true},{quoted: mek})
-       m.react("🤡")
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG + botwatermark})
+       m.react("✔")
 }catch(e){
 console.log(e)
 reply(`${e}`)
