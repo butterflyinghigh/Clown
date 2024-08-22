6e7358eadc0ff26f3aed944bcf808fbfa34cb74d
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-22 10:48:47 -0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -23,7 +23,7 @@ try{
 
 for (let i = 0; i < commands.length; i++) {
 if (commands[i].pattern && !commands[i].dontAddCommandList) {
-menu[commands[i].category] += `${prefix}${commands[i].pattern}\n`;
+menu[commands[i].category] += `.${commands[i].pattern}\n`;
  }
 }
 
