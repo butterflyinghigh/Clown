5047e3aee6b3fb3594359f6af39a6841c7e0ae46
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-22 11:47:55 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,7 +10,7 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-       await conn.sendMessage(from,{audio:{url: `https://od.lk/d/NTdfOTMxMzEyOTlf/Hi.%20i%20am%20clown%20md%20whatsapp%20user%20bot%20created%20by%20mr%20unknown.mp3`},mimetype:"audio/mpeg",ptt:true},{quoted: mek})
+       await conn.sendMessage(from,{audio:{url: config.ALIVE_AUDIO},mimetype:"audio/mpeg",ptt:true},{quoted: mek})
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG + botwatermark})
 }catch(e){
 console.log(e)
