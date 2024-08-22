0f808e158ab7ad38309239d1eda9ddda8570ddc9
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-22 08:02:57 -0700

    Create ai.js

---
+++ plugins/ai.js
@@ -0,0 +1,24 @@
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const { fetchJson } = require('../lib/functions')
+const { botwatermark } = require('../botwatermark')
+
+cmd({
+    pattern: "gpt",
+    alias: ["ai,chatgpt"]
+    desc: "Chat with ai",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+  m.react("🤖")
+data = await fetchJson(`https://chatgptforprabath-md.vercel.app/api/gptv1?q=${q}`)
+  reply(`${data.data}\n\n\n` + botwatermark)
+  return await m.react("✅")
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
