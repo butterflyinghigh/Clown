c5e998da1e837b9811bcaaa6ec2195afc3271d29
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-22 08:45:55 -0700

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,7 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "2Ms1zbaA#wP2T5Cxnu2lCuz65SQ5KFs8BK6C-JPWJkcbDDiMO5VI",
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94755845165",
+CONNECT_MSG: process.env.CONNECT_MSG || "> *🤖 Clown-MD Whatsapp Bot Connected ✅*\n\n*PREFIX:* ${prefix}\n*OWNER NUMBER:* ${ownerNumber}\n*WORK TYPE:* ${MODE}\n*ALIVE IMG:* ${ALIVE_IMG}\n*ALIVE AUDIO:* ${ALIVE_AUDIO}\n*AUTO READ STATUS:* ${AUTO_READ_STATUS}\n*WORK TYPE:* ${MODE}\n*O REACT:* ${O_REACT}\n*OWNER REACT:* ${OWNER_REACT}",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://telegra.ph/file/9e0a59ba5c8cd8c292741.jpg",
 ALIVE_MSG: process.env.ALIVE_MSG || "╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾\n*Hi 👋*\n╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾\n*I'm Clown-MD Whatsapp Bot 🤡*\n*I'm Alive Now! ✅*\n*How Can I Help You? 🤔*\n╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾╾\n\n\n",
 ALIVE_AUDIO: process.env.ALIVE_AUDIO || "https://od.lk/d/NTdfOTMxMzEyOTlf/Hi.%20i%20am%20clown%20md%20whatsapp%20user%20bot%20created%20by%20mr%20unknown.mp3",
