60c740d532d38e3da179d4805a2efb9bc27ee3d0
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-22 11:12:38 -0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -12,7 +12,6 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-    m.react("📃")
   let menu = {
     main: '',
     download: '',
@@ -46,7 +45,9 @@ ${menu.group}
 > *Search Commands 🔎*
 ${menu.search}\n\n\n${botwatermark}`
 
-  await conn.sendMessage(from,{image: {url: `https://od.lk/d/NTdfOTMyMjU4MzRf/menu-restaurant-hand-drawn-lettering-phrase-text-vector-illustration-inscription-on-white-background-calligraphy-for-the-design-of-posters-card.jpg`},caption: madeMenu},{quoted:mek})
+m.react("📃")
+await conn.sendMessage(from,{audio: {url: `https://od.lk/d/NTdfOTMyMjU4NDlf/here%20is%20the%20clown%20md%20bot%20command%20list.wav`},mimetype:"audio/mpeg",ptt:true}{quoted:mek})
+await conn.sendMessage(from,{image: {url: `https://od.lk/d/NTdfOTMyMjU4MzRf/menu-restaurant-hand-drawn-lettering-phrase-text-vector-illustration-inscription-on-white-background-calligraphy-for-the-design-of-posters-card.jpg`},caption: madeMenu},{quoted:mek})
 
 }catch(e){
 console.log(e)
