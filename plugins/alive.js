e57d2ec763b9e9670f9f58218b565508e10235ec
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-24 07:34:07 -0700

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -1,4 +1,4 @@
-const config = require('../config')
+const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 const { botwatermark } = require('../botwatermark')
 
@@ -10,8 +10,9 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+const config = await readEnv();
 await m.react("👋")
-        await conn.sendMessage(from,{audio:{url: config.ALIVE_AUDIO},mimetype:"audio/mpeg",ptt:true},{quoted: mek})
+       await conn.sendMessage(from,{audio:{url: config.ALIVE_AUDIO},mimetype:"audio/mpeg",ptt:true},{quoted: mek})
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG + "\n\n\n" + botwatermark})
 }catch(e){
 console.log(e)
