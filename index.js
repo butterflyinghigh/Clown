58d8af3577575ec235395a17ad696062de5f59eb
Mr-Unknown-SL <nuwanbot11@gmail.com>
2024-08-24 22:42:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -80,7 +80,7 @@ console.log('✅ Clown-MD Bot Connected')
 
 let up = `> *🤡 Cʟᴏᴡɴ-MD Wʜᴀᴛꜱᴀᴘᴘ Bᴏᴛ Cᴏɴɴᴇᴄᴛᴇᴅ ✅*\n\n*PREFIX:* ${config.PREFIX}\n*OWNER_NUMBER:* ${config.OWNER_NUMBER}\n*MODE:* ${config.MODE}\n*O_REACT:* ${config.O_REACT}\n*OWNER_REACT:* ${config.OWNER_REACT}\n*AUTO_READ_STATUS:* ${config.AUTO_READ_STATUS}\n*ALIVE_AUDIO:* ${config.ALIVE_AUDIO}\n*ALIVE_IMG:* ${config.ALIVE_IMG}\n*ALIVE_MSG:* ${config.ALIVE_MSG}\n\n\n` + botwatermark;
 
-conn.sendMessage(ownerNumber + "@s.whatsapp.net", { image: { url: `https://od.lk/d/NTdfOTMyMjc0ODVf/20240823_095407.png` }, caption: up })
+conn.sendMessage(config.OWNER_NUMBER + "@s.whatsapp.net", { image: { url: `https://od.lk/d/NTdfOTMyMjc0ODVf/20240823_095407.png` }, caption: up })
 
 }
 })
